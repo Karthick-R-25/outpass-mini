e8d18f2e2425ef8412493e2e7998e1c84d7c895c
Karthick-R-25 <karthickr2429@gmail.com>
2025-08-25 19:52:30 +0530

    Update react new version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import {UserProvider} from './components/userFile/UserContext'
+import { UserProvider } from './components/userFile/UserContext'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <App />
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
